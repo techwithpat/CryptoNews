7547d89f6c9ef8231094579d256845390409b806
patrick <patrick@makanda.io>
2022-05-06 16:48:11 +0200

    Add project files.

---
+++ CryptoNews/News/NewsDto.cs
@@ -0,0 +1,44 @@
+﻿namespace CryptoNews.News
+{    
+    public class NewsDto
+    {
+        public Datum[] data { get; set; }
+    }
+
+    public class Datum
+    {
+        public string screen_ID { get; set; }
+        public Screen_Data screen_data { get; set; }
+        public string screen_layout { get; set; }
+    }
+
+    public class Screen_Data
+    {
+        public string ob { get; set; }
+        public int next_page { get; set; }
+        public News[] news { get; set; }
+    }
+
+    public class News
+    {
+        public int news_ID { get; set; }
+        public string hash { get; set; }
+        public string providerId { get; set; }
+        public string itemCategoryTags { get; set; }
+        public string news_provider_name { get; set; }
+        public string last_updated { get; set; }
+        public int last_updated_uts { get; set; }
+        public string type { get; set; }
+        public string HEADLINE { get; set; }
+        public string BODY { get; set; }
+        public string news_link { get; set; }
+        public string third_party_url { get; set; }
+        public string related_image { get; set; }
+        public string related_image_big { get; set; }
+        public string vid_filename { get; set; }
+        public string comments_cnt { get; set; }
+        public string is_partial { get; set; }
+        public string itemType { get; set; }
+    }
+
+}
---
+++ CryptoNews/News/NewsService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace CryptoNews.News
+{
+    public class NewsService : INewsService
+    {
+        private readonly HttpClient _httpClient;
+        const string _baseUrl = "https://investing-cryptocurrency-markets.p.rapidapi.com/";
+        const string _newsEndpoint = "coins/get-news?pair_ID=1057391&page=1&time_utc_offset=28800&lang_ID=1";
+        const string _host = "investing-cryptocurrency-markets.p.rapidapi.com";
+        const string _key = "YOURKEY";
+
+        public NewsService(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async Task<List<NewsItem>> GetNews()
+        {
+            ConfigureHttpClient();
+
+            var response = await _httpClient.GetAsync(_newsEndpoint);
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            var dto = await JsonSerializer.DeserializeAsync<NewsDto>(stream);
+            return dto.data.FirstOrDefault().screen_data.news.Select(n => new NewsItem 
+            {
+                Headline = n.HEADLINE,
+                Body = (MarkupString)n.BODY,
+                ImageUrl = n.related_image
+            }).ToList();
+        }
+
+        private void ConfigureHttpClient()
+        {
+            _httpClient.BaseAddress = new Uri(_baseUrl);
+            _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host", _host);
+            _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key", _key);
+        }
+    }
+}
